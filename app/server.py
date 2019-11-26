b507edefcf06f98911afcae395d606741748fac4
zhiying-dsta <58208336+zhiying-dsta@users.noreply.github.com>
2019-11-26 16:24:51 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = '/content/pokemon/export.pkl'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['pikachu', 'charmander', 'bulbasaur', 'squirtle']
 path = Path(__file__).parent
 
 app = Starlette()
